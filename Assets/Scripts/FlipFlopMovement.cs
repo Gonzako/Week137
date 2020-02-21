72de4cbb1b8b3206680cec09af3f9c9e33f313ac
Gonzako <al341522@uji.es>
2020-02-21 21:23:12 +0100

    added draft of game

--- Assets/Scripts/FlipFlopMovement.cs
+++ Assets/Scripts/FlipFlopMovement.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class FlipFlopMovement : MonoBehaviour
 {
+
+    public feetState state;
+
     public float massAsAnchor;
     public float massAsFeet;
 
     private Rigidbody2D rb;
     private TargetJoint2D mouseJoint;
+    private SpringJoint2D anchorJoint;
 
     private void Start()
     {
+        anchorJoint = GetComponent<SpringJoint2D>();
         rb = GetComponent<Rigidbody2D>();
         mouseJoint = GetComponent<TargetJoint2D>();
     }
 
     public void makeAnchor()
     {
+        state = feetState.anchor;
         rb.mass = massAsAnchor;
+        anchorJoint.enabled = false;
         mouseJoint.enabled = false;
     }
 
     public void makeFeet()
     {
+        anchorJoint.enabled = true;
+        state = feetState.feet;
         rb.mass = massAsFeet;
         mouseJoint.enabled = true;
     }
+}
+
+public enum feetState
+{
+    anchor, feet
 }
---
+++ Assets/anchorSwitcher.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anchorSwitcher : MonoBehaviour
+{
+    public static Action<FlipFlopMovement[], FlipFlopMovement> onStateSwitch;
+    public static Action<FlipFlopMovement[], lastButtonUsed> onWrongSwitch;
+
+    private lastButtonUsed lastButton = lastButtonUsed.rightClick;
+    private FlipFlopMovement[] flipFlops;
+    private Vector2 inputVector;
+
+    private void Start()
+    {
+        flipFlops = GetComponentsInChildren<FlipFlopMovement>();
+    }
+
+    private void switchStates()
+    {
+        foreach (FlipFlopMovement n in flipFlops)
+        {
+            switch (n.state)
+            {
+                case feetState.anchor:
+                    n.makeFeet();
+                    break;
+                case feetState.feet:
+                    onStateSwitch?.Invoke(flipFlops, n);
+                    n.makeAnchor();
+                    break;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (lastButton == lastButtonUsed.leftClick)
+            {
+                Debug.Log("badLeftClick");
+                onWrongSwitch?.Invoke(flipFlops, lastButton);
+            }
+            inputVector.y = 1;
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if(lastButton == lastButtonUsed.rightClick)
+            {
+                Debug.Log("badRightClick");
+                onWrongSwitch?.Invoke(flipFlops, lastButton);
+            }
+            inputVector.x = 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch (lastButton)
+        {
+            case lastButtonUsed.rightClick:
+                if(inputVector.y == 1)
+                {
+                    switchStates();
+                    lastButton = lastButtonUsed.rightClick;
+                }
+                break;
+            case lastButtonUsed.leftClick:
+                if (inputVector.x == 1)
+                {
+                    switchStates();
+                    lastButton = lastButtonUsed.leftClick;
+                }
+                break;
+        }
+        clearInput();
+
+    }
+
+    private void clearInput()
+    {
+        inputVector = Vector2.zero;
+    }
+
+    public enum lastButtonUsed
+    {
+        rightClick, leftClick
+    }
+}
