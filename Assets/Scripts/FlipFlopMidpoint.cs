7b0148fe1e716dd00106560d3df89dab9f58a19f
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2020-02-22 18:34:52 +0000

    camera

---
+++ Assets/Scripts/FlipFlopMidpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipFlopMidpoint : MonoBehaviour
+{
+    private GameObject flip;
+    private GameObject flop;
+
+    void Start()
+    {
+        flip = GameObject.FindGameObjectWithTag("Flip");
+        flop = GameObject.FindGameObjectWithTag("Flop");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = (flip.transform.position + flop.transform.position) / 2;
+    }
+}
