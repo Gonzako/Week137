f1993b6bd644a34f612561de40ff671ea3d025b3
Gonzako <al341522@uji.es>
2020-02-23 17:09:45 +0100

    added googly eyes to tell up from down

---
+++ Assets/Scripts/setDistanceJointToFatherAnchor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setDistanceJointToFatherAnchor : MonoBehaviour
+{
+    private DistanceJoint2D googlyAnchor;
+    void Start()
+    {
+
+        googlyAnchor = GetComponent<DistanceJoint2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        googlyAnchor.connectedAnchor = transform.parent.position;
+    }
+}
