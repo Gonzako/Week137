c6781c29a5db0043a832d58fc6bd64ea2b18113e
Gonzako <al341522@uji.es>
2020-02-23 17:46:31 +0100

    Update sizeAnimator.cs

--- Assets/Scripts/sizeAnimator.cs
+++ Assets/Scripts/sizeAnimator.cs
@@ -7,7 +7,7 @@ using DG.Tweening;
 public class sizeAnimator : MonoBehaviour
 {
     public bool isUsingRect = false;
-    public Vector3 maxSize = Vector3.one*2;
+    public Vector3 sizeToAdd = Vector3.one*2;
     public float timeToMax = 0.1f, timeToNormal = 0.4f;
     public Ease maxEase, normalEase;
 
@@ -25,13 +25,13 @@ public class sizeAnimator : MonoBehaviour
         sequence = DOTween.Sequence();
         if (!isUsingRect)
         {
-            sequence.Append(transform.DOScale(maxSize, timeToNormal).SetEase(maxEase))
+            sequence.Append(transform.DOScale(defaultSize + sizeToAdd, timeToNormal).SetEase(maxEase))
                 .Append(transform.DOScale(defaultSize, timeToNormal).SetEase(normalEase));
         }
         else
         {
             var rect = GetComponent<RectTransform>();
-            sequence.Append(DOTween.To(() => rect.localScale, x => rect.localScale = x, maxSize, timeToMax).SetEase(maxEase))
+            sequence.Append(DOTween.To(() => rect.localScale, x => rect.localScale = x, defaultSize + sizeToAdd, timeToMax).SetEase(maxEase))
                     .Append(DOTween.To(() => rect.localScale, x => rect.localScale = x, defaultSize, timeToNormal).SetEase(normalEase));
         }
     }
