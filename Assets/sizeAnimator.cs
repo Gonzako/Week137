8e8ba70a813e264ecdc889e2fd068333424c6aa0
Gonzako <al341522@uji.es>
2020-02-23 16:12:14 +0100

    Made quick animation for Mouse Promtp

---
+++ Assets/sizeAnimator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class sizeAnimator : MonoBehaviour
+{
+    public bool isUsingRect = false;
+    public Vector3 maxSize = Vector3.one*2;
+    public float timeToMax = 0.1f, timeToNormal = 0.4f;
+    public Ease maxEase, normalEase;
+
+
+    private Vector3 defaultSize;
+    private Sequence sequence;
+
+    private void Awake()
+    {
+        defaultSize = transform.localScale;
+    }
+
+    public void AnimateSize()
+    {
+        sequence = DOTween.Sequence();
+        if (!isUsingRect)
+        {
+            sequence.Append(transform.DOScale(maxSize, timeToNormal).SetEase(maxEase))
+                .Append(transform.DOScale(defaultSize, timeToNormal).SetEase(normalEase));
+        }
+        else
+        {
+            var rect = GetComponent<RectTransform>();
+            sequence.Append(DOTween.To(() => rect.localScale, x => rect.localScale = x, maxSize, timeToMax).SetEase(maxEase))
+                    .Append(DOTween.To(() => rect.localScale, x => rect.localScale = x, defaultSize, timeToNormal).SetEase(normalEase));
+        }
+    }
+
+}
