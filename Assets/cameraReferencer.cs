c16a740402ca72241f174ca1a848a1a7334d6e51
Gonzako <al341522@uji.es>
2020-02-24 14:25:59 +0100

    added camera Refernecer script for game UI prefab

---
+++ Assets/cameraReferencer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraReferencer : MonoBehaviour
+{
+    public string cameraTag = "MainCamera";
+
+    private void Awake()
+    {
+        var canvas = GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = GameObject.FindGameObjectWithTag(cameraTag).GetComponent<Camera>();
+    }
+}
