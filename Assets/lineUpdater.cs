ae7d26da31e4ac2733510138c2c41c33b7cf0a7a
Gonzako <al341522@uji.es>
2020-02-25 12:59:13 +0100

    Added draft of distance line in level 1

---
+++ Assets/lineUpdater.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Bindings;
+
+public class lineUpdater : MonoBehaviour
+{
+    public Color colorAtMaxPosition;
+    public Color colorAtNonMax;
+    public float maxDistance = 1.3f;
+    public Transform startPoint;
+    public Transform endPoint;
+
+
+    private LineRenderer lR;
+
+    private void Awake()
+    {
+        lR = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lR.SetPosition(0, startPoint.position);
+        lR.SetPosition(1, endPoint.position);
+        if (Vector3.Distance(startPoint.position, endPoint.position) > maxDistance)
+        {
+
+            lR.startColor = colorAtMaxPosition;
+            lR.endColor = colorAtMaxPosition;
+        }
+        else
+        {
+            lR.startColor = colorAtNonMax;
+            lR.endColor = colorAtNonMax;
+        }
+    }
+}
