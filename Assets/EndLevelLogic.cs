cd8c4756bed7d3f10186e5ea6464003872df879a
Gonzako <al341522@uji.es>
2020-02-25 18:12:40 +0100

    added endLevel logic

---
+++ Assets/EndLevelLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+using UnityEngine.SceneManagement;
+
+public class EndLevelLogic : MonoBehaviour
+{
+    public GameEvent endLevelGE;
+    public float timeToNextLevel = 0.2f;
+    private AsyncOperation loadOp;
+    private const string doorTag = "DoorTag";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DoorTag"))
+        {
+            loadOp = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+            loadOp.allowSceneActivation = false;
+            endLevelGE.Raise();
+            Invoke("NextLevel", timeToNextLevel);
+        }
+    }
+
+    private void NextLevel()
+    {
+        loadOp.allowSceneActivation = true;
+    }
+}
